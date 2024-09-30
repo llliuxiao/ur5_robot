9e8453cb282933000a35014db3234a85514919d2
LiuXiao <xiao.liu.2001@outlook.com>
2024-09-30 16:09:56 +0800

    remove useless files, modify from state publisher to robot_state_publisher in pkg robot_state_publisher, recover origin license and set own license as BSD

--- ur5_gazebo/scripts/send_gripper.py
+++ ur5_gazebo/scripts/send_gripper.py
@@ -1,48 +1,28 @@
-#!/usr/bin/python
-#
-# Send a value to change the opening of the Robotiq gripper using an action
-#
-
 import argparse
-
 import rospy
 import actionlib
 import control_msgs.msg
 
 
 def gripper_client(value):
-
-    # Create an action client
-    client = actionlib.SimpleActionClient(
-        '/gripper_controller/gripper_cmd',  # namespace of the action topics
-        control_msgs.msg.GripperCommandAction # action type
-    )
-    
-    # Wait until the action server has been started and is listening for goals
+    client = actionlib.SimpleActionClient('/gripper_controller/gripper_cmd', control_msgs.msg.GripperCommandAction)
     client.wait_for_server()
-
-    # Create a goal to send (to the action server)
     goal = control_msgs.msg.GripperCommandGoal()
-    goal.command.position = value   # From 0.0 to 0.8
-    goal.command.max_effort = -1.0  # Do not limit the effort
-    client.send_goal(goal)
-
-    client.wait_for_result()
+    goal.command.position = value
+    goal.command.max_effort = -1.0
+    client.send_goal_and_wait(goal)
     return client.get_result()
 
 
 if __name__ == '__main__':
     try:
-        # Get the angle from the command line
         parser = argparse.ArgumentParser()
         parser.add_argument("--value", type=float, default="0.2",
-                            help="Value betwewen 0.0 (open) and 0.8 (closed)")
+                            help="Value between 0.0 (open) and 0.8 (closed)")
         args = parser.parse_args()
         gripper_value = args.value
-        # Start the ROS node
         rospy.init_node('gripper_command')
-        # Set the value to the gripper
         result = gripper_client(gripper_value)
-        
+
     except rospy.ROSInterruptException:
-        print ("Program interrupted before completion")
+        print("Program interrupted before completion")
